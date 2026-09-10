04fda3547be880d12e81243f1e01c34bba34ae31
unknown <2amod@e041edu.local>
2026-09-10 15:04:17 -0300

    feat: Configurando o button (NIcolas/Matheus)

--- LojaDoces/Pedido.cs
+++ LojaDoces/Pedido.cs
@@ -3,5 +3,37 @@ namespace LojaDoces
 {
     internal class Pedido
     {
+        public string? NomeDoce {  get; set; }
+        public double PrecoUnitario {  get; set; }
+        public int Quantidade {  get; set; }
+        public int IdadeCliente {  get; set; }
+
+        public double CalcularDesconto()
+        {
+            double valorTotal = PrecoUnitario * Quantidade;
+            if (Quantidade > 10)
+            {
+                return valorTotal * 0.10;
+            }
+            return 0.0;
+
+         }
+
+        public double CalcularValorFinal()
+        {
+            double valorTotalBruto = PrecoUnitario * Quantidade;
+            double desconto = CalcularDesconto();
+            return valorTotalBruto = desconto;
+        }
+
+        public double CalcularParcela()
+        {
+            return CalcularValorFinal() / 3;
+        }
+
+        public bool ValidarBrinde()
+        {
+            return (IdadeCliente >= 18 && Quantidade >= 5 || CalcularValorFinal() > 100);
+        }
     }
 }
